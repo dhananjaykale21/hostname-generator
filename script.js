323abfdaf6910b1ab2fa3308a9fe528fe08483e8
dhananjaykale21 <83104065+dhananjaykale21@users.noreply.github.com>
2026-06-18 21:17:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+let historyList = [];
 
+function generateHostname() {
+
+    let hostname =
+        document.getElementById("region").value +
+        document.getElementById("platform").value +
+        document.getElementById("resource").value +
+        document.getElementById("purpose").value +
+        document.getElementById("project").value.toLowerCase() +
+        document.getElementById("environment").value +
+        String(document.getElementById("number").value).padStart(2, '0');
+
+    document.getElementById("result").innerText = hostname;
+
+    historyList.push(hostname);
+
+    renderHistory();
+}
+
+function renderHistory() {
+
+    let ul = document.getElementById("history");
+    ul.innerHTML = "";
+
+    historyList.forEach(item => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        ul.appendChild(li);
+    });
+}
+
+function copyHostname() {
+
+    let hostname = document.getElementById("result").innerText;
+
+    navigator.clipboard.writeText(hostname);
+
+    alert("Hostname copied!");
+}
+
+function exportCSV() {
+
+    let csv = historyList.join("\n");
+
+    let blob = new Blob([csv], {type:'text/csv'});
+
+    let a = document.createElement("a");
+
+    a.href = URL.createObjectURL(blob);
+
+    a.download = "hostname_history.csv";
+
+    a.click();
+}
+
+function toggleDarkMode() {
+
+    document.body.classList.toggle("dark");
+}
